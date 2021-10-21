91de3414d0d8197e81b56ab5b558e5b0fa40408c
Richard Hallows <jeddy3@users.noreply.github.com>
2021-10-21 17:23:13 +0100

    Prepare 2.0.0 (#1)

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,160 +1,70 @@
-"use strict";
+'use strict';
 
-const config = require("../");
-const fs = require("fs");
-const stylelint = require("stylelint");
+const config = require('../');
+const fs = require('fs');
+const stylelint = require('stylelint');
 
-const validCss = fs.readFileSync("./__tests__/css-valid.css", "utf-8");
-const invalidCss = fs.readFileSync("./__tests__/css-invalid.css", "utf-8");
-const validScss = fs.readFileSync("./__tests__/scss-valid.scss", "utf-8");
-const invalidScss = fs.readFileSync("./__tests__/scss-invalid.scss", "utf-8");
+const validScss = fs.readFileSync('./__tests__/valid.scss', 'utf-8');
+const invalidScss = fs.readFileSync('./__tests__/invalid.scss', 'utf-8');
 
-describe("flags no warnings with valid css", () => {
-  let result;
+describe('flags no warnings with valid scss', () => {
+	let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: validCss,
-      config
-    });
-  });
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: validScss,
+			config,
+		});
+	});
 
-  it("did not error", () => {
-    return result.then(data => expect(data.errored).toBeFalsy());
-  });
+	it('did not error', () => {
+		return result.then((data) => expect(data.errored).toBeFalsy());
+	});
 
-  it("flags no warnings", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(0)
-    );
-  });
+	it('flags no warnings', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(0));
+	});
 });
 
-describe("flags no warnings with valid scss", () => {
-  let result;
+describe('flags warnings with invalid scss', () => {
+	let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: validScss,
-      config,
-      syntax: "scss",
-    });
-  });
+	beforeEach(() => {
+		result = stylelint.lint({
+			code: invalidScss,
+			config,
+		});
+	});
 
-  it("did not error", () => {
-    return result.then(data => expect(data.errored).toBeFalsy());
-  });
+	it('did error', () => {
+		return result.then((data) => expect(data.errored).toBeTruthy());
+	});
 
-  it("flags no warnings", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(0)
-    );
-  });
-});
+	it('flags one warning', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(1));
+	});
 
-describe("flags warnings with invalid css", () => {
-  let result;
+	it('correct warning text', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].text).toBe('Expected variable to be kebab-case'),
+		);
+	});
 
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: invalidCss,
-      config
-    });
-  });
+	it('correct rule flagged', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[0].rule).toBe('scss/dollar-variable-pattern'),
+		);
+	});
 
-  it("did error", () => {
-    return result.then(data => expect(data.errored).toBeTruthy());
-  });
+	it('correct severity flagged', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
+	});
 
-  it("flags one warning", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(1)
-    );
-  });
+	it('correct line number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].line).toBe(1));
+	});
 
-  it("correct warning text", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].text).toBe(
-        'Expected quotes (function-url-quotes)'
-      )
-    );
-  });
-
-  it("correct rule flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].rule).toBe("function-url-quotes")
-    );
-  });
-
-  it("correct severity flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].severity).toBe("error")
-    );
-  });
-
-  it("correct line number", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].line).toBe(2)
-    );
-  });
-
-  it("correct column number", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].column).toBe(19)
-    );
-  });
-});
-
-describe("flags warnings with invalid scss", () => {
-  let result;
-
-  beforeEach(() => {
-    result = stylelint.lint({
-      code: invalidScss,
-      config,
-      syntax: "scss",
-    });
-  });
-
-  it("did error", () => {
-    return result.then(data => expect(data.errored).toBeTruthy());
-  });
-
-  it("flags one warning", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings).toHaveLength(1)
-    );
-  });
-
-  it("correct warning text", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].text).toBe(
-        'Expected $ variable name to match specified pattern (scss/dollar-variable-pattern)'
-      )
-    );
-  });
-
-  it("correct rule flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].rule).toBe("scss/dollar-variable-pattern")
-    );
-  });
-
-  it("correct severity flagged", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].severity).toBe("error")
-    );
-  });
-
-  it("correct line number", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].line).toBe(1)
-    );
-  });
-
-  it("correct column number", () => {
-    return result.then(data =>
-      expect(data.results[0].warnings[0].column).toBe(1)
-    );
-  });
+	it('correct column number', () => {
+		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(1));
+	});
 });
